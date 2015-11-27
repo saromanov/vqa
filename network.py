499bc5e6d5227daf67b7097339f4763e035fe11d
Sergey Romanov <xxsmotur@gmail.com>
2015-11-27 19:21:22 +0500

    Update Recurrent model

--- network.py
+++ network.py
@@ -11,7 +11,7 @@ class Recurrent:
 		self.hid_dim
 		self.word_dim = word_dim
 
-	def train(self, modelname='GRU'):
+	def train(self, X, y, Xtest, ytest, modelname='GRU', batch_size=32):
 		model = Sequential()
 		if modelname == 'GRU':
 			model.add(GRU(output_dim=self.hid_dim, return_sequence=True, input_shape=(maxvalue, self.word_dim)))
@@ -19,5 +19,10 @@ class Recurrent:
 			model.add(LSTM(output_dim=self.hid_dim, return_sequence=True, input_shape=(maxvalue, self.word_dim)))
 		else:
 			raise Exception("Model name is not found")
+		model.add(Dropout(0.5))
 		model.add(Activation('sigmoid'))
 		model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+		model.fit(X,y, batch_size=batch_size, nb_epoch=4, validation_data=(Xtest, ytest), show_accuracy=True)
+		score, acc = model.evaluate(Xtest, ytest, batch_size=batch_size, show_accuracy=True)
+		print('Score: {0}'.format(score))
+		print('Accuracy: {0}'.format(acc))
